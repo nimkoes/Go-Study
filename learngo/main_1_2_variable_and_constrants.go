3d74abecea3fd38ac10a39ece35a9522127383c1
nimkoes <xxxelppa@gmail.com>
2021-05-16 11:49:52 +0900

    Go 의 변수와 상수

---
+++ learngo/main_1_2_variable_and_constrants.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const name string = "nimkoes"
+
+	var nickname string = "nickname"
+	fmt.Println(nickname)
+
+	another := "another"
+	fmt.Println(another)
+}
